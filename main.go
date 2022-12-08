d560a078b2a1aa838519874a5726b85fd6fb1fec
John Hashem III <johnhashemiii@Johns-MacBook-Pro-2.local>
2022-12-08 17:21:11 -0500

    added go html template for homepage

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/httplog"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	tpl, err := template.ParseFiles("templates/home.gohtml")
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.",
+			http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.",
+			http.StatusInternalServerError)
+		return
+	}
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,14 +55,36 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jh@gmail.com\">jh@gmail.com</a>.")
 }
 
+func MyRequestHandler(w http.ResponseWriter, r *http.Request) {
+	// fetch the url parameter `"userID"` from the request of a matching
+	// routing pattern. An example routing pattern could be: /users/{userID}
+	userID := chi.URLParam(r, "userID")
+
+	// respond to the client
+	w.Write([]byte(fmt.Sprintf("Here is the userID: %v", userID)))
+}
+
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
 // }
 
 func main() {
+
+	logger := httplog.NewLogger("httplog", httplog.Options{
+		// JSON: true,
+		Concise: true,
+		// Tags: map[string]string{
+		// 	"version": "v1.0-81aa4244d9fc8076a",
+		// 	"env":     "dev",
+		// },
+	})
+
 	r := chi.NewRouter()
+	r.Use(httplog.RequestLogger(logger))
+	r.Use(middleware.Heartbeat("/ping"))
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.Get("/articles/{userID}", MyRequestHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
